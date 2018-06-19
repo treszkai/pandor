e042d51cee573527edc4d34b2c506f753241b348
Laszlo Treszkai <laszlo.treszkai@gmail.com>
2018-06-19 22:22:21 +0100

    New backtracking magic

--- source/pandor.py
+++ source/pandor.py
@@ -2,6 +2,8 @@ import logging
 from collections import OrderedDict
 
 import time
+from itertools import dropwhile
+from itertools import product
 
 
 class PandorBacktrackException(Exception):
@@ -67,9 +69,12 @@ class WalkAB(Environment):
 
     @staticmethod
     def str_action(a):
-        if a == -1:  return "Left"
-        elif a == 1: return "Right"
-        else: assert False, f"Nonsense action: {a}"
+        if a == -1:
+            return "Left"
+        elif a == 1:
+            return "Right"
+        else:
+            assert False, f"Nonsense action: {a}"
 
     @staticmethod
     def str_obs(o):
@@ -162,8 +167,11 @@ class MealyController:
 class AndOrPlanner:
     def __init__(self, env):
         self.env = env
+        # attributes used by synth_plan() - def'd here only to suppress warnings
+        self.contr, self.backtracking, self.backtrack_stack = None, None, None
 
     def synth_plan(self, bound):
+        self.backtracking = False
         self.backtrack_stack = []
         self.contr = MealyController(bound)
         # Note: this creates a controller first for one init state,
@@ -179,82 +187,135 @@ class AndOrPlanner:
     #       (the and_step didn't even make the recursion step clearer)
     #       (and_step is needed for synth_plan though)
     def and_step(self, q, sl_next, history):
-        for s in sl_next:
-            logging.info("Simulating s:%s, q:%s", self.env.str_state(s), q)
-            self.or_step(q, s, history)
+
+        def get_backtracked_iterator():
+            # don't care about or_steps that succeeded and to which we don't want to backtrack
+            # history == self.backtrack_stack[-1][0:len(history)]
+            # so the relevant element of sl_next is
+            #  self.backtrack_stack[-1][len(history)]
+            return dropwhile(lambda x: x != self.backtrack_stack[-1][len(history)],
+                             iter(sl_next))
+
+        if self.backtracking:
+            it = get_backtracked_iterator()
+        else:
+            it = iter(sl_next)
+
+        while True:
+            try:
+                s_k = next(it)
+            except StopIteration:
+                return True
+
+            if self.backtracking:
+                logging.info("AND: Redoing s: %s", self.env.str_state(s_k))
+            else:
+                logging.info("AND: Simulating s: %s, q: %s", self.env.str_state(s_k), q)
+
+            # Note: only need to copy the history if len(sl_next) > 1. I guess.
+            #       Or alternatively, could save len(history) and clip it after each new or_step
+            #       (like we do in or_step already after backtracking)
+            if not self.or_step(q, s_k, history[:]):
+                self.backtracking = True
+                # decide if we should backtrack left or up
+                # (ignore the last element of self.backtrack_stack[-1])
+                if history == self.backtrack_stack[-1][:min(len(history), len(self.backtrack_stack[-1])-1)]:
+                    it = get_backtracked_iterator()
+                    logging.info("AND: Backtracking left")
+                else:
+                    logging.info("AND: Backtracking up")
+                    return False
 
     def or_step(self, q, env_state, history):
-        if env_state in self.env.goal_states:
-            return
+        if not self.backtracking:
+            if env_state in self.env.goal_states:
+                return True
 
-        if (q, env_state) in history:
-            raise PandorBacktrackException
+            if (q, env_state) in history:
+                return False
 
-        history.append((q, env_state))
-        obs = self.env.get_obs(env_state)
+            history.append((q, env_state))
+            obs = self.env.get_obs(env_state)
 
-        if (q, obs) in self.contr.transitions:
-            q_next, action = self.contr[q, obs]
-            if action not in self.env.legal_actions(env_state):
-                raise PandorBacktrackException
+            if (q, obs) in self.contr.transitions:
+                q_next, action = self.contr[q, obs]
+                if action not in self.env.legal_actions(env_state):
+                    return False
+
+                sl_next = self.env.next_states(env_state, action)
+                self.and_step(q_next, sl_next, history)
+                return True
+
+            # no (q_next,act) defined for (q,obs) ⇒ define new one with this iterator
+            it = product(range(self.contr.num_states+1),
+                         self.env.legal_actions(env_state))
+
+            # store a new checkpoint iff we're not backtracking currently
+            self.backtrack_stack.append(history[:])
+            logging.info("OR: checkpoint at q: %s, s: %s\n    with history %s\n",
+                         q, self.env.str_state(env_state), history)
+
+        else:  # backtracking
+            history.append((q, env_state))
+            obs = self.env.get_obs(env_state)
+
+            t = self.contr.transitions.popitem()
+            logging.info("OR: (redoing) Deleted: (%s,%s) -> (%s,%s)",
+                         t[0][0], self.env.str_obs(t[0][1]),
+                         t[1][0], self.env.str_action(t[1][1]))
+
+            q_next_last = t[1][0]
+            action_last = t[1][1]
+
+            it = dropwhile(lambda x: x[1] != action_last,
+                           product(range(q_next_last, self.contr.num_states + 1),
+                                   self.env.legal_actions(env_state)))
+
+            # this is the node of the last checkpoint
+            # (enough to check the length because we're in the right branch now)
+            if len(history) == len(self.backtrack_stack[-1][history]):
+                self.backtracking = False
+                # burn the controller extension that caused the trouble earlier
+                _ = next(it)
+
+        # non-det branching of q',a
+
+        # save function arguments for bracktracking (history is already in backtrack_stack)
+        # Note: we have to make a shallow copy of the history b/c we modify it during recursion
+        # Note: as the state transitions of the controller are ordered according to their
+        #       being added, then we needn't store it in backtrack_stack.
+
+        env_state_saved = env_state
+        q_saved = q
+
+        # important: q_next first, so we only add new states when necessary
+        for q_next, action in it:
+            # extend controller
+            self.contr[q, obs] = q_next, action
+            logging.info("OR: Added:   (%s,%s) -> (%s,%s)",
+                         q, self.env.str_obs(obs),
+                         q_next, self.env.str_action(action))
+
             sl_next = self.env.next_states(env_state, action)
-            self.and_step(q_next, sl_next, history)
-            return
 
-        else:  # no (q_next,act) defined for (q,obs) ⇒ define new one
-            # non-det branching of q',a
+            if self.and_step(q_next, sl_next, history):
+                return True
+            else:
+                # restore saved values
+                q, env_state = q_saved, env_state_saved
+                len_history = len(self.backtrack_stack[-1])
 
-            # save function arguments for bracktracking
-            # Note: we don't have to make a (full) checkpoint when either
-            #       there's only one legal action or we're adding a new state - but who cares
-            # Note: we have to make a shallow (?) copy of the arguments that we modify during recursion
-            #       i.e. controller and history
-            # Note: for memory efficiency, can store len(history) instead of history
-            # Note: if the state transitions of the controller were ordered according to their
-            #       being added, then we needn't make a copy of it either.
-            # Note: memory - env_state need not be saved either, I guess
-            self.backtrack_stack.append((q, env_state, len(history)))
-            logging.debug("%s\n", self.backtrack_stack)
+                logging.info("OR: Backstep: %s",
+                             [(q, self.env.str_state(s)) for q, s in history[-1:len_history-1:-1]])
+                # it's enough to clip the history if we're backtracking in the or_step
+                del history[len_history:]
 
-            # important: q_next first, so we only add new states when necessary
-            for q_next in range(self.contr.num_states+1):
-                for action in self.env.legal_actions(env_state):
-                    # extend controller
-                    self.contr[q, obs] = q_next, action
-                    logging.info("Added:   (%s,%s) -> (%s,%s)",
-                                 q, self.env.str_obs(obs),
-                                 q_next, self.env.str_action(action))
+                t = self.contr.transitions.popitem()
+                logging.info("OR: Deleted: (%s,%s) -> (%s,%s)",
+                             t[0][0], self.env.str_obs(t[0][1]),
+                             t[1][0], self.env.str_action(t[1][1]))
 
-                    sl_next = self.env.next_states(env_state, action)
-
-                    try:
-                        self.and_step(q_next, sl_next, history)
-                        return
-                    except PandorBacktrackException:
-                        q, env_state, len_history = self.backtrack_stack[-1]
-                        logging.info("Backstep: %s",
-                                     [(q,self.env.str_state(s))
-                                            for q,s in history[-1:len_history-1:-1]])
-                        history = history[:len_history]
-
-                        # TODO XXX As a result of the above line, some function calls will see a different list than others do
-                        # (And in fact, this history clipping doesn't carry through AND steps.)
-                        # Use a single history, common to the Planner object
-                        # Is it correct if in the AND_step we save len_history and clip the history after the or_step succeeds?
-                        # What if I use
-                        #   del history[len_history:] ?
-                        # Same result, just no new object.
-                        # And do the history clipping in AND.
-                        # Why did it work in Prolog? I guess it passes arguments by value. Why am I messing with this again?
-                        # But even if I did that, this algo wouldn't backtrack over AND nodes – why?
-
-                        t = self.contr.transitions.popitem()
-                        logging.info("Deleted: (%s,%s) -> (%s,%s)",
-                                     t[0][0], self.env.str_obs(t[0][1]),
-                                     t[1][0], self.env.str_action(t[1][1]))
-
-            assert False, "Are we ever here?"
-
+        return False
 
 
 if __name__ == '__main__':
